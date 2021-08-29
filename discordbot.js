99948b80e159b575618db359d6f38ea16e0eade2
1ntegrale9 <1ntegrale9uation@gmail.com>
2021-08-29 18:19:46 +0900

    [Fix] eslintrcを更新

--- discordbot.js
+++ discordbot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (msg) => {
+client.on('messageCreate', msg => {
   if (msg.content === '/ping') {
     msg.reply('pong');
   }
