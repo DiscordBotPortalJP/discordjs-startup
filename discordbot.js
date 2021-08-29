e9284eae0c9922676a0c84f483c645c566b8e08d
1ntegrale9 <1ntegrale9uation@gmail.com>
2021-08-29 18:16:50 +0900

    [Fix] サンプルコードを更新

--- discordbot.js
+++ discordbot.js
@@ -1,12 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
 const token = process.env.DISCORD_BOT_TOKEN;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', (msg) => {
+client.on('messageCreate', (msg) => {
   if (msg.content === '/ping') {
     msg.reply('pong');
   }
